0a93b2b9de241a67751f2e058bb2ec51387686ca
valeria_sanchez_c@hotmail.com <valeria_sanchez_c@hotmail.com>
2022-05-28 07:44:34 -0400

    deployment

--- proyecto1/settings.py
+++ proyecto1/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = 'django-insecure-=e55cq8*y067m9$wpndtm#5=^c!yfxb&pb@l)3nv!!_)5uhj6f
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-#ALLOWED_HOSTS = ['proyectoscuola.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['trazaeventos.herokuapp.com']
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.cookie.CookieStorage"
 
